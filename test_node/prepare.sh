654a80f6bd7df71bfec5abae2e38def8a0c2879f
Michael C. Grant <mcg@cvxr.com>
2019-02-20 17:11:24 -0600

    Improving test node generator

--- test_node/prepare.sh
+++ test_node/prepare.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 for user in *; do
   if [[ -d "$user" ]]; then
+    echo "User: $user"
     pushd "$user" >/dev/null
     for project in *; do
       if [[ -d "$project" ]]; then
+        echo "  Project: $project"
         pushd "$project" >/dev/null
-        for env in *.yml; do
-          prefix=envs/${env%.yml}
-          if [[ -d $prefix ]]; then
-            echo "Environment $user/$project/envs/$prefix" already exists
-          elif [[ $env != *.yml ]]; then
-            echo "Creating $user/$project/envs/$prefix"
-            conda env create -f $env -p $prefix
-          fi
-        done
+        if compgen -G "*.yml" >/dev/null; then
+          for env in *.yml; do
+            echo "    Environment: ${env%.yml}"
+            prefix=envs/${env%.yml}
+            if [[ ! -d $prefix ]]; then
+              conda env create -q -f $env -p $prefix 2>&1 | sed 's@^@      @'
+            else
+              echo "      environment already exists"
+            fi
+          done
+        fi
         if [[ -f prepare.sh ]]; then
-          echo "Running $user/$project/prepare.sh"
+          echo "  Running $user/$project/prepare.sh"
           bash prepare.sh </dev/null
         fi
         popd >/dev/null
